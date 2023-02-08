caaa856babef972319a60b29a50156b7987b42fd
Amal sebastian <amalpsebastian98@gmail.com>
2023-02-08 19:16:34 +0000

    Docstrings added

--- scrapper.py
+++ scrapper.py
@@ -13,14 +13,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Aim:Define methods to go through the website and collect all Top 20 cryptocurrencies and their historical data
-#Aim:To store this data after cleaning and plotting , locally and remote in different servers
-#Aim:Find the Standard deviation and correlation of the data
 
 class CoinmarketcapScraper:
 
     def __init__(self):
-#Initialising the scraper with default url ,driver, all required parameters
+        
+        """
+        Initialize the class with the required attributes.
+
+        Attributes:
+            url (str): The URL of the CoinMarketCap website.
+            driver (webdriver.Chrome): The Chrome web driver to be used for browsing.
+            wait (WebDriverWait): The WebDriverWait object to wait for elements to load.
+            chrome_options (Options): The Chrome options for the web driver.
+        """
+
         self.url = "https://coinmarketcap.com"
         self.driver = webdriver.Chrome("///home/amalsebastian/Downloads/chromedriver_linux64/chromedriver") 
         self.wait = WebDriverWait(self.driver, 10)
@@ -34,31 +41,47 @@ class CoinmarketcapScraper:
         links, names = self.fetch_data()
         df_final = self.process_data(links, names)
         
-        
 
-
-#METHOD TO GET THE DATA CLEARING ALL POPUPS AND LOCATING TABLE
     def fetch_data(self):
-#closing all the pop ups and cookies 
+        """ 
+        Gets the main table of contents.
+        This method is used to clear all pop ups and cookies
+        and get the main table of contents.This is used to get links and names of all required number of cryptocurrencies
+
+        Parameters:
+        None
+
+        Returns:
+        Links:List of links to the historic trading data of each currency
+        names:List of Names of all respective currencies
+
+        Raise :
+        TimeoutError:If the main table couldnt be fetched
+        Exception e:Prints Popup cleared if popup doesnt exist / is closed
+        """
         self.driver.get(self.url)        
-        """ try:
-            close_button = self.wait.until(EC.presence_of_element_located((By.XPATH, "//*/span[contains(text(),'Maybe later')]")))
+        try:
+            close_button = self.wait.until(EC.presence_of_element_located\
+                ((By.XPATH, "//*/span[contains(text(),'Maybe later')]")))
             close_button.click()
-        except TimeoutError:
-            print("TimeoutError: Could not find close button")
+        except Exception as e:
+            print("Popup Cleared")
         time.sleep(4)
-        if self.driver.find_elements_by_xpath("//div[@id='cmc-cookie-policy-banner']//div[@class='cmc-cookie-policy-banner__close']"):
-                cookie_button = self.driver.find_element_by_xpath("//div[@id='cmc-cookie-policy-banner']//div[@class='cmc-cookie-policy-banner__close']")
-                cookie_button.click() """
-#Waiting for the Main table to appear
+
+        if self.driver.find_elements_by_xpath("//div[@id='cmc-cookie-policy-banner']\
+            //div[@class='cmc-cookie-policy-banner__close']"):
+                cookie_button = self.driver.find_element_by_xpath("//div[@id='cmc-cookie-policy-banner']\
+                    //div[@class='cmc-cookie-policy-banner__close']")
+                cookie_button.click()
         try:
             table = self.wait.until(EC.presence_of_element_located((By.XPATH, "//table/tbody")))
         except TimeoutError:
             print("TimeoutError: Could not find table")
+
         tr_tags = self.driver.find_elements_by_xpath("//table/tbody/tr")
         links = []
         names = []
-#selecting and creating a list of links and names of cryptocurrencies to access
+
         for i in range(3):
             a_tag = tr_tags[i].find_element_by_xpath(".//a")
             name = a_tag.text
@@ -68,30 +91,39 @@ class CoinmarketcapScraper:
             names = [name.split('\n')[0] for name in names]
         return links, names
 
-
-
-#METHOD TO PROCESS THE DATA TO A DATA FRAME
     def process_data(self, links, names):
+        """
+        Processes the data from given links and returns a concatenated dataframe.
+
+        This method takes in a list of links and names as inputs, loops through each link, accesses the link,
+        scrapes the historical data table, processes the data and finally concatenates the processed data 
+        into a final dataframe which is returned as the output.
+
+        Parameters:
+        links (list): List of links from which data needs to be processed
+        names (list): List of names corresponding to the links
+
+        Returns:
+        pd.DataFrame: Concatenated dataframe of processed data.
+
+        """
         data = []
         df_final = pd.DataFrame()
         for i in range(len(links)):
-# Go to the historical data page of the currency
-            #print("Accessing : ", names[i])
             time.sleep(2)
             self.driver.get(links[i] + "historical-data/")
-#Add scrolling to the driver
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(1)
             self.driver.execute_script("window.scrollTo(0, 0);")
             time.sleep(2)
-#creating a try and except to double check the presence of the single crypto table
             try:
-                self.wait.until(EC.presence_of_element_located((By.XPATH, "//table[contains(@class, cm)]")))
+                self.wait.until(EC.presence_of_element_located\
+                    ((By.XPATH, "//table[contains(@class, cm)]")))
             except TimeoutError:
                 print("TimeoutError: Could not find historical data table")
             historical_table = self.driver.find_element("xpath", "//table")
             row_list = historical_table.find_elements("xpath",".//tr")
-#creating a for loop to get the specific data from the table
+
             data = []
             for row in row_list:
                 cols = row.find_elements("xpath", ".//td")
@@ -99,39 +131,48 @@ class CoinmarketcapScraper:
                     date = cols[0].text
                     close_price = cols[4].text
                     data.append([date, close_price])
-#creating data frame for the date and closing price
-            df = pd.DataFrame(data, columns=["Date",names[i]])
 
-# Removing the "$" symbol from the "amount" column and converting it to float
+            df = pd.DataFrame(data, columns=["Date",names[i]])
             df = df.replace({"\$": ""}, regex=True)
             
             df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
             df[df.columns[1:]] = df[df.columns[1:]].replace(',', '', regex=True).astype(float)
-#concatenating the closing price and deleting the date 
+
             df_final = pd.concat([df_final,df],axis =1)
             df_final = df_final.T.drop_duplicates().T
 
         return df_final
 
     def save_file(self,df_final):
-# Create the raw_data folder if it doesn't exist
+        """
+        This method saves the processed data into a file in json format.
+        The data is saved in a folder structure as follows:
+
+        raw_data/YYYY-MM-DD/data.json
+
+        If the folder `raw_data` does not exist, it will be created.
+        Similarly, a folder with the current date (YYYY-MM-DD) will be created if it does not exist.
+        The data is saved in a file named `data.json` in the date folder.
+
+        Args:
+        df_final (pandas.DataFrame): The processed data to be saved.
+
+        Returns:
+        None
+        """
 
         raw_data_folder = 'raw_data'
         if not os.path.exists(raw_data_folder):
             os.makedirs(raw_data_folder)
 
-# Create a folder with the current date as its name
         now = datetime.datetime.now()
         date_folder = now.strftime("%Y-%m-%d")
         date_folder_path = os.path.join(raw_data_folder, date_folder)
         if not os.path.exists(date_folder_path):
             os.makedirs(date_folder_path)
 
-# Save the dataframe in a file called data.json in the date folder
         data_file = os.path.join(date_folder_path, "data.json")
         df_final.to_json(data_file, orient='records')
-
-
 
 
 #Methods to plot the data 
